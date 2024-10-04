df6430a2480ee98ff90081597d2e945a85cb52be
Mahdi_Ghanbarpour <mahdi10ghanbarpour@gmail.com>
2024-10-04 14:38:01 +0330

    Add files via upload

---
+++ mahdi.py
@@ -0,0 +1,80 @@
+# try:
+#  z="hjdhhh"
+#  z.swapcase("reaza")
+# except TypeError:
+#     print("eshtebahh")
+# try:
+#     print(6/0)
+#     try:
+#         print(n)
+#     except NameError as mw:
+#         print(mw)
+# except ZeroDivisionError as be :
+# #     print(be)
+# def hello ():
+#     return 'hello2'
+# s=hello()
+# # print(s
+# def f(x,u):
+#    if x > u:
+#        return x
+#    return u
+# r=f(2,6)
+# # print(r)
+# def zarb(x,y):
+#    print(x*y)
+# def jam(x,y):
+#     print(x+y)
+# def taghsim(x,y):
+#     print(x/y)
+# def tavvan(x,y):
+#     print(x**y)
+# def manfi(x,y):
+#     print(x-y)
+# def voroudi():
+#     x=int(input("enter ..."))
+#     y=input("enter...")
+#     z=int(input("enter ..."))
+#     if y == "+":
+#         return jam(x,z)
+#     elif y == "*":
+#         return zarb(x,z)
+#     elif y == "/":
+#         return taghsim(x,z)
+#     elif y == "*":
+#         return tavvan(x,z)
+# voroudi()
+# def f(x,y):
+#     if x > y :
+#         return x
+#     return y
+#
+# r= f(2,5)
+# # print(r)
+# def f(x,y,z)
+#     return f(x , f(y,z))
+#
+# print(g(2,5,3))
+x= 1
+
+def f ():
+    global x
+    x=2
+    print(x)
+f()
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
