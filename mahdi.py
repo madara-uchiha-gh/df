0d36ba72985315ab3f0293af1f5c595eb0c47bd1
Mahdi_Ghanbarpour <mahdi10ghanbarpour@gmail.com>
2024-10-25 00:05:15 +0330

    Update mahdi.py

--- mahdi.py
+++ mahdi.py
@@ -1,26 +1,4 @@
-# try:
-#  z="hjdhhh"
-#  z.swapcase("reaza")
-# except TypeError:
-#     print("eshtebahh")
-# try:
-#     print(6/0)
-#     try:
-#         print(n)
-#     except NameError as mw:
-#         print(mw)
-# except ZeroDivisionError as be :
-# #     print(be)
-# def hello ():
-#     return 'hello2'
-# s=hello()
-# # print(s
-# def f(x,u):
-#    if x > u:
-#        return x
-#    return u
-# r=f(2,6)
-# # print(r)
+
 # def zarb(x,y):
 #    print(x*y)
 # def jam(x,y):
@@ -43,27 +21,9 @@
 #         return taghsim(x,z)
 #     elif y == "*":
 #         return tavvan(x,z)
-# voroudi()
-# def f(x,y):
-#     if x > y :
-#         return x
-#     return y
-#
-# r= f(2,5)
-# # print(r)
-# def f(x,y,z)
-#     return f(x , f(y,z))
-#
-# print(g(2,5,3))
-x= 1
-
-def f ():
-    global x
-    x=2
-    print(x)
-f()
-print(x)
-
+#     elif y=="-" 
+#         return manfi(x,z) 
+#voroudi() 
 
 
 
